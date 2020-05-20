d0d31977d59872cecd34850254208336bf550d36
Manoel Ruiz Morales <manoelruizmorales@gmail.com>
2020-05-20 02:05:20 -0500

    Depurar base e incluir función

---
+++ Database/dbo/Functions/spfncGetMenu.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE FUNCTION spfncGetMenu
+(	
+	-- Add the parameters for the function here
+	@mes as int
+	
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	-- Add the SELECT statement with parameter references here
+
+	SELECT cast(@mes as nvarchar(5))Mes
+)
